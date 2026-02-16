95080b317bcdc6a6121e26384f0a99be7caae17c
Lukas Spiss <lukas@spiss.dev>
2026-02-16 21:10:37 +0000

    chore: prep for pre-built binary

--- Formula/shlf.rb
+++ Formula/shlf.rb
@@ -1,16 +1,14 @@
 class Shlf < Formula
   desc "Tiny macOS menu bar app that keeps your folder close"
   homepage "https://github.com/spissable/shlf"
-  url "https://github.com/spissable/shlf/archive/refs/tags/v1.2.2.tar.gz"
+  url "https://github.com/spissable/shlf/releases/download/v1.2.2/shlf-macos.tar.gz"
   sha256 "88b6018540368c3276f108268ae786d9ed441ab91752ac468af3cdb55e3615ec"
   license "MIT"
 
-  depends_on xcode: ["15.0", :build]
   depends_on :macos
 
   def install
-    system "swift", "build", "-c", "release", "--disable-sandbox"
-    bin.install ".build/release/shlf"
+    bin.install "shlf"
   end
 
   test do
